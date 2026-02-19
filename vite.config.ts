9ebdfcbea66f51cacfd8d92b71c36152c7ba6dfb
Serhat Demir <serhatdemir1235@gmail.com>
2026-02-20 00:35:35 +0300

    r

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/brief/", // alt dizin kullanmıyorsan bu satırı sil
+  base: "/loveclick-brief/",
+  build: {
+    outDir: "docs",
+  },
 })
